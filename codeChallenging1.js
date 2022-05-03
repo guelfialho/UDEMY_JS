30c2ff3320f8ad226d088d703fb45dbae58dbe61
Miguel Fialho <fialhomig@gmail.com>
2022-05-03 17:58:13 -0300

    Challenge Code 1

---
+++ codeChallenging1.js
@@ -0,0 +1,30 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+// GOOD LUCK �
+
+johnMass = 85; // kg
+johnHeight = 1.66; // meters
+
+marksMass = 95; //kg
+marksHeight = 1.88; // meters
+
+johnBMI = johnMass / (johnHeight * johnHeight);
+marksBMI = marksMass / (marksHeight * marksHeight);
+
+markHigherBMI = marksBMI > johnBMI;
+
+console.log(johnBMI, marksBMI);
+console.log(markHigherBMI);
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let js = "amazing";
-if (js === "amazing") {
-  alert("Javascript is FUN");
-}
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log(40 + 8 + 23 - 10);
+// console.log(23);
+
+// let firstName = "Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+let country = "Brazil";
+let continent = "South America";
+let population = "220M";
+
+console.log("Country: ", country);
+console.log("Population: ", population);
+console.log("Continent: ", continent);
