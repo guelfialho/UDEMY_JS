e545e11449725c6b22d72be58ccf9601ef89add9
Miguel Fialho <fialhomig@gmail.com>
2022-05-17 09:52:14 -0300

    Implementing dynamic style

--- S5_DOMandEVENTS/Project1_GessMyNumber/script.js
+++ S5_DOMandEVENTS/Project1_GessMyNumber/script.js
@@ -1,27 +1,40 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number inserted';
+    // When the player wins
   } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // When the guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High';
-    score -= 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+    // when the guess is too low
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
-    score -= 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
-
-console.log(score);
