ef89da7fe9998248d0f10e5f4af1cc092cb5dc6d
Miguel Fialho <fialhomig@gmail.com>
2022-05-23 08:52:42 -0300

    Finishing section 10

--- S10_FUNCTIONS/Challenge1/codeChallenge1.js
+++ S10_FUNCTIONS/Challenge1/codeChallenge1.js
@@ -1,5 +1,40 @@
 'use strict';
 
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(`What is your favourite programming language?
+//   0: JavaScript
+//   1: Python
+//   2: Rust
+//   3: C++
+//   (Write option number)`)
+//     );
+//     if (answer >= 0 && answer <= 3) {
+//       poll.answers[answer]++;
+//     }
+//     const play = poll.displayResults;
+//     play.call(poll, 'string');
+//   },
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       const results = this.answers;
+//       console.log(`Poll results are ${results}.`);
+//     }
+//   },
+// };
+
+// const btnPoll = document.querySelector('.poll');
+// btnPoll.addEventListener('click', poll.registerNewAnswer);
+
+// jonas code :
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
@@ -8,26 +43,34 @@ const poll = {
 
   registerNewAnswer() {
     const answer = Number(
-      prompt(`What is your favourite programming language?
-  0: JavaScript
-  1: Python
-  2: Rust
-  3: C++
-  (Write option number)`)
+      prompt(
+        `${this.question}\n ${this.options.join('\n')}\n (Write option number)`
+      )
     );
-    if (answer >= 0 && answer <= 3) {
-      poll.answers[answer]++;
-    }
+    console.log(answer);
+
+    // register
+
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      answer >= 0 &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
   },
+
   displayResults(type = 'array') {
-    if (type === 'array') {
+    if (type == 'array') {
       console.log(this.answers);
-    } else if (type === 'string') {
-      const results = this.answers;
-      console.log(`Poll results are ${results}.`);
+    } else if (type == 'string') {
+      console.log(`Poll results are ${this.answers}.`);
     }
   },
 };
 
-const btnPoll = document.querySelector('.poll');
-btnPoll.addEventListener('click', poll.registerNewAnswer);
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
---
+++ S10_FUNCTIONS/Challenge2/codeChallenge2.js
@@ -0,0 +1,16 @@
+'use strict';
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  let i = 0;
+
+  document.querySelector('body').addEventListener('click', function () {
+    if (i === 0) {
+      header.style.color = 'blue';
+      i = 1;
+    } else if (i === 1) {
+      header.style.color = 'red';
+      i = 0;
+    }
+  });
+})();
