33f1805305a4d1f30e659c67906762434ac754e4
Miguel Fialho <fialhomig@gmail.com>
2022-05-24 14:58:03 -0300

    Constructor Function Inheritance

--- S14_OOP/script.js
+++ S14_OOP/script.js
@@ -1,22 +1,47 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
-  // Intance properties
-
   this.firstName = firstName;
   this.birthYear = birthYear;
-  //   this.calcAge = function () {
-  //     console.log(2022 - this.birthYear);
-  //   }; // using like this we have the same function(method) stored in each intance of the Person. Is cases like this we use Person.prototype and use prototype inheritance and chain to store in only one place.
 };
 
 Person.prototype.calcAge = function () {
   console.log(2022 - this.birthYear);
-}; // with propotype inheritance we only have one function that all intances of a class (Person) have access to use, whithout have to create the same method  for each class instance in the constructor.
+};
 
-const jonas = new Person('Jonas', 1991);
-const miguel = new Person('Miguel', 1998);
-const luna = new Person('Luna', 2021);
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype); // linking prototypes
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 1990, 'Computer Science');
+
+mike.introduce();
+mike.calcAge();
+
+// const Person = function (firstName, birthYear) {
+//   // Intance properties
+
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+//   //   this.calcAge = function () {
+//   //     console.log(2022 - this.birthYear);
+//   //   }; // using like this we have the same function(method) stored in each intance of the Person. Is cases like this we use Person.prototype and use prototype inheritance and chain to store in only one place.
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(2022 - this.birthYear);
+// }; // with propotype inheritance we only have one function that all intances of a class (Person) have access to use, whithout have to create the same method  for each class instance in the constructor.
+
+// const jonas = new Person('Jonas', 1991);
+// const miguel = new Person('Miguel', 1998);
+// const luna = new Person('Luna', 2021);
 
 // console.log(jonas);
 // console.log(miguel);
@@ -36,32 +61,32 @@ const luna = new Person('Luna', 2021);
 // const PersonCl = class {}
 
 // class declaration =
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  // method will be added to the propotype, not to intances
-  calcAge() {
-    console.log(2022 - this.birthYear);
-  }
+// method will be added to the propotype, not to intances
+//   calcAge() {
+//     console.log(2022 - this.birthYear);
+//   }
 
-  greet() {
-    return console.log(`Hello, ${this.fullName}!`);
-  }
+//   greet() {
+//     return console.log(`Hello, ${this.fullName}!`);
+//   }
 
-  set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a fullName!`);
-  }
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a fullName!`);
+//   }
 
-  // get fullName() {
-  //   return this._fullName;
-  // }
-}
+//   // get fullName() {
+//   //   return this._fullName;
+//   // }
+// }
 
-const jessica = new PersonCl('Jessica', 1996);
+// const jessica = new PersonCl('Jessica', 1996);
 
 // console.log(jessica);
 
